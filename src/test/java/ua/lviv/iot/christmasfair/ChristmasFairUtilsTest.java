28fb84c6065ed2af872cfef6f53c73204bdfce05
Raven <redstoyningbros@gmail.com>
2020-02-08 14:42:53 +0200

    Rewrited sorting tests. Changed from automatic test to manual.

--- src/test/java/ua/lviv/iot/christmasfair/ChristmasFairUtilsTest.java
+++ src/test/java/ua/lviv/iot/christmasfair/ChristmasFairUtilsTest.java
@@ -19,61 +19,51 @@ class ChristmasFairUtilsTest extends BaseChristmasFairTest {
 	}
 
 	@Test
-	void testSortDecorationsByPriceAscending() {
+	void testSortDecorationsByPriceAscending() {	
 		List<AbstractDecor> sortedDecorations = ChristmasFairUtils.sortDecorationsByPrice(decorations, SortType.ASCENDING);
-		double previousPrice = 0;
-		for (AbstractDecor currentDecoration : sortedDecorations) {
-			if(previousPrice == 0) {
-				previousPrice = currentDecoration.getPriceInHryvnas();
-			}
-			assertTrue(currentDecoration.getPriceInHryvnas() >= previousPrice);
-			previousPrice = currentDecoration.getPriceInHryvnas();
-		}
+		assertEquals(50.25, sortedDecorations.get(0).getPriceInHryvnas());
+		assertEquals(75.0, sortedDecorations.get(1).getPriceInHryvnas());
+		assertEquals(105.0, sortedDecorations.get(2).getPriceInHryvnas());
+		assertEquals(110.0, sortedDecorations.get(3).getPriceInHryvnas());
+		assertEquals(120.50, sortedDecorations.get(4).getPriceInHryvnas());
+		assertEquals(145.0, sortedDecorations.get(5).getPriceInHryvnas());
+		assertEquals(145.0, sortedDecorations.get(6).getPriceInHryvnas());
 	}
 	
 	@Test
 	void testSortDecorationsByPriceDescending() {
 		List<AbstractDecor> sortedDecorations = ChristmasFairUtils.sortDecorationsByPrice(decorations, SortType.DESCENDING);
-		double previousPrice = 0;
-		for (AbstractDecor currentDecoration : sortedDecorations) {
-			if(previousPrice == 0) {
-				previousPrice = currentDecoration.getPriceInHryvnas();
-			}
-			assertTrue(currentDecoration.getPriceInHryvnas() <= previousPrice);
-			previousPrice = currentDecoration.getPriceInHryvnas();
-		}
+		assertEquals(145.0, sortedDecorations.get(0).getPriceInHryvnas());
+		assertEquals(145.0, sortedDecorations.get(1).getPriceInHryvnas());
+		assertEquals(120.50, sortedDecorations.get(2).getPriceInHryvnas());
+		assertEquals(110.0, sortedDecorations.get(3).getPriceInHryvnas());
+		assertEquals(105.0, sortedDecorations.get(4).getPriceInHryvnas());
+		assertEquals(75.0, sortedDecorations.get(5).getPriceInHryvnas());
+		assertEquals(50.25, sortedDecorations.get(6).getPriceInHryvnas());
 	}
 
 	@Test
 	void testSortDecorationsByColorAscending() {
 		List<AbstractDecor> sortedDecorations = ChristmasFairUtils.sortDecorationsByColor(decorations, SortType.ASCENDING);
-		char[] previousColor = null;
-		char[] currentColor = null;
-		for (AbstractDecor currentDecoration : sortedDecorations) {
-			if(previousColor == null) {
-				previousColor = currentDecoration.getColor().toCharArray();
-			}
-			currentColor = currentDecoration.getColor().toCharArray();
-			
-			assertTrue(currentColor[0] >= previousColor[0]);
-			previousColor = currentDecoration.getColor().toCharArray();
-		}
+		assertEquals("Blue", sortedDecorations.get(0).getColor());
+		assertEquals("Green", sortedDecorations.get(1).getColor());
+		assertEquals("Red", sortedDecorations.get(2).getColor());
+		assertEquals("Red", sortedDecorations.get(3).getColor());
+		assertEquals("Yellow", sortedDecorations.get(4).getColor());
+		assertEquals("Yellow-Red", sortedDecorations.get(5).getColor());
+		assertEquals("Yellow-Red", sortedDecorations.get(6).getColor());
 	}
 	
 	@Test
 	void testSortDecorationsByColorDescending() {
 		List<AbstractDecor> sortedDecorations = ChristmasFairUtils.sortDecorationsByColor(decorations, SortType.DESCENDING);
-		char[] previousColor = null;
-		char[] currentColor = null;
-		for (AbstractDecor currentDecoration : sortedDecorations) {
-			if(previousColor == null) {
-				previousColor = currentDecoration.getColor().toCharArray();
-			}
-			currentColor = currentDecoration.getColor().toCharArray();
-			
-			assertTrue(currentColor[0] <= previousColor[0]);
-			previousColor = currentDecoration.getColor().toCharArray();
-		}
+		assertEquals("Yellow-Red", sortedDecorations.get(0).getColor());
+		assertEquals("Yellow-Red", sortedDecorations.get(1).getColor());
+		assertEquals("Yellow", sortedDecorations.get(2).getColor());
+		assertEquals("Red", sortedDecorations.get(3).getColor());
+		assertEquals("Red", sortedDecorations.get(4).getColor());
+		assertEquals("Green", sortedDecorations.get(5).getColor());
+		assertEquals("Blue", sortedDecorations.get(6).getColor());
 	}
 
 }
