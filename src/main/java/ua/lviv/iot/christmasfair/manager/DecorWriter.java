b48c0a11fb1affdd39c4a96975d959d11ef76a2f
Raven <redstoyningbros@gmail.com>
2020-02-25 18:12:59 +0200

    Created DecorWriter to write objects to file

---
+++ src/main/java/ua/lviv/iot/christmasfair/manager/DecorWriter.java
@@ -0,0 +1,29 @@
+package ua.lviv.iot.christmasfair.manager;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import ua.lviv.iot.christmasfair.model.AbstractDecor;
+
+public class DecorWriter {
+	public void writeToFile(List<AbstractDecor> decorations, String filePath) {
+		File file = new File(filePath);
+		writeToFile(decorations, file);
+	}
+
+	public void writeToFile(List<AbstractDecor> decorations, File file) {
+		try (Writer textWriter = new FileWriter(file);) {
+			for (AbstractDecor currentDecor : decorations) {
+				writeLine(textWriter, currentDecor.getHeaders() + ", " + currentDecor.toCSV());
+			}
+		} catch (IOException e) {
+			
+		}
+	}
+	
+	private void writeLine(Writer writer, String text) throws IOException {
+		writer.write(text + "\r\n");
+	}
+}
