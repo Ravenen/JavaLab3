1a56de49b9d64d241d9f91975603be385dddfbcc
Raven <redstoyningbros@gmail.com>
2020-02-25 22:11:55 +0200

    Made methods static, because it`s no need to create object of this class

--- src/main/java/ua/lviv/iot/christmasfair/manager/DecorWriter.java
+++ src/main/java/ua/lviv/iot/christmasfair/manager/DecorWriter.java
@@ -8,22 +8,22 @@ import java.util.List;
 import ua.lviv.iot.christmasfair.model.AbstractDecor;
 
 public class DecorWriter {
-	public void writeToFile(List<AbstractDecor> decorations, String filePath) {
+	public static void writeToFile(List<AbstractDecor> decorations, String filePath) {
 		File file = new File(filePath);
-		writeToFile(decorations, file);
+		DecorWriter.writeToFile(decorations, file);
 	}
 
-	public void writeToFile(List<AbstractDecor> decorations, File file) {
+	public static void writeToFile(List<AbstractDecor> decorations, File file) {
 		try (Writer textWriter = new FileWriter(file);) {
 			for (AbstractDecor currentDecor : decorations) {
-				writeLine(textWriter, currentDecor.getHeaders() + ", " + currentDecor.toCSV());
+				DecorWriter.writeLine(textWriter, currentDecor.getHeaders() + ", " + currentDecor.toCSV());
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
 	}
 	
-	private void writeLine(Writer writer, String text) throws IOException {
+	private static void writeLine(Writer writer, String text) throws IOException {
 		writer.write(text + "\r\n");
 	}
 }
