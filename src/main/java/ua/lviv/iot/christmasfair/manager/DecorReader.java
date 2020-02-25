c28b6149ca26e1e55ae9b76fd6dd49b782cf437c
Raven <redstoyningbros@gmail.com>
2020-02-25 22:12:37 +0200

    Created reader for objects

---
+++ src/main/java/ua/lviv/iot/christmasfair/manager/DecorReader.java
@@ -0,0 +1,29 @@
+package ua.lviv.iot.christmasfair.manager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DecorReader {
+
+	public static List<String> readLinesFromFile(String path) {
+		File file = new File(path);
+		return DecorReader.readLinesFromFile(file);
+	}
+
+	public static List<String> readLinesFromFile(File file) {
+		List<String> lines = new LinkedList<String>();
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));) {
+			String currentLine = null;
+			while ((currentLine = bufferedReader.readLine()) != null) {
+				lines.add(currentLine);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lines;
+	}
+}
