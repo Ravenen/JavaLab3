8c7db573b9b0edb7d05b769db84555a15399057b
Raven <redstoyningbros@gmail.com>
2020-02-27 13:26:37 +0200

    Changed writer to do it more universal

--- src/main/java/ua/lviv/iot/christmasfair/manager/DecorWriter.java
+++ src/main/java/ua/lviv/iot/christmasfair/manager/DecorWriter.java
@@ -1,32 +1,35 @@
 package ua.lviv.iot.christmasfair.manager;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
+
 import ua.lviv.iot.christmasfair.model.AbstractDecor;
 
 public class DecorWriter {
-	public static void writeToFile(List<AbstractDecor> decorations, String filePath) {
-		File file = new File(filePath);
-		DecorWriter.writeToFile(decorations, file);
+
+	Writer textWriter;
+
+	public void setTextWriter(Writer textWriter) {
+		this.textWriter = textWriter;
 	}
 
-	public static void writeToFile(List<AbstractDecor> decorations, File file) {
-		try (Writer textWriter = new FileWriter(file, StandardCharsets.UTF_8);) {
-			for (AbstractDecor currentDecor : decorations) {
-				String lineToWrite = currentDecor.getHeaders() + ", " + currentDecor.toCSV();
-				DecorWriter.writeLine(textWriter, lineToWrite);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
+	public void writeToFile(List<AbstractDecor> decorations) throws IOException {
+		for (AbstractDecor currentDecor : decorations) {
+			String lineToWrite = currentDecor.getHeaders() + ", " + currentDecor.toCSV();
+			writeLine(textWriter, lineToWrite);
+		}
+		textWriter.flush();
 	}
-	
-	private static void writeLine(Writer writer, String text) throws IOException {
+
+	private void writeLine(Writer writer, String text) throws IOException {
 		writer.write(text + "\r\n");
 	}
+
+	@Override
+	public String toString() {
+		return textWriter.toString();
+	}
+	
+	
 }
